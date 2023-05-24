be24f4e0cf2f7f5f3f8f3eba6563b0bd3e464a0f
Rijvi <93908058+rijvimahmudd@users.noreply.github.com>
2023-05-24 18:44:42 +0600

    work on navbar

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { CssBaseline } from '@mui/material';
+import Navbar from './components/navbar';
+import './App.css';
+
+function App() {
+	return (
+		<>
+			<CssBaseline />
+			<Navbar></Navbar>
+		</>
+	);
+}
+
+export default App;
---
+++ src/components/navbar/index.jsx
@@ -0,0 +1,69 @@
+import { Box, Container, Stack, Typography } from '@mui/material';
+
+const menuLinks = [
+	{
+		title: 'home',
+		link: '/',
+	},
+	{
+		title: 'project',
+		link: '/',
+	},
+	{
+		title: 'contact',
+		link: '/',
+	},
+	{
+		title: 'about',
+		link: '/',
+	},
+];
+
+const Navbar = () => {
+	return (
+		<>
+			<Box
+				sx={{
+					height: '70px',
+					// backgroundColor: 'orange',
+				}}
+			>
+				<Container
+					sx={{
+						alignItems: 'center',
+						display: 'flex',
+						// alignContent: 'center',
+						height: '100%',
+					}}
+				>
+					<Typography
+						variant="h5"
+						sx={{ flexGrow: 1 }}
+						fontWeight={'bold'}
+						fontFamily={'macondo'}
+						letterSpacing={1.5}
+					>
+						Rijvi Mahmud.
+					</Typography>
+					<Stack direction={'row'} spacing={3}>
+						{menuLinks.map((item, index) => {
+							return (
+								<Typography
+									key={index}
+									variant="overline"
+									fontWeight={'light'}
+									sx={{ cursor: 'pointer' }}
+								>
+									{' '}
+									{item.title.toUpperCase()}{' '}
+								</Typography>
+							);
+						})}
+					</Stack>
+				</Container>
+			</Box>
+		</>
+	);
+};
+
+export default Navbar;
